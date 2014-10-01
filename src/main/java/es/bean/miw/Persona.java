6e055f0d0bbe58027131812aeb952f7819edc87e
guille209 <gguillemiranda@gmail.com>
2014-10-01 09:56:09 +0200

    CR1.04

--- src/main/java/es/bean/miw/Persona.java
+++ src/main/java/es/bean/miw/Persona.java
@@ -3,7 +3,7 @@ package es.bean.miw;
 public class Persona {
 private String dni;
 private String nombre;
-private String apellido;
+private String apellidos;
 
 public Persona(String dni,String nombre){
     this.dni = dni;
